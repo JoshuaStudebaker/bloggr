20f7adbb460c2f3166ade3c54bc0b3f1d752c2a9
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-19 11:52:23 -0600

    first commit

---
+++ src/services/AxiosService.js
@@ -0,0 +1,13 @@
+import Axios from "axios";
+export const api = Axios.create({
+  baseURL: "https://cw-bloggr.herokuapp.com/api/",
+  timeout: 3000
+});
+
+export const setBearer = function (bearer) {
+  api.defaults.headers.authorization = bearer;
+};
+
+export const resetBearer = function () {
+  api.defaults.headers.authorization = "";
+};
---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import router from "../router";
+import { api } from "../services/AxiosService.js";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    profile: {},
+  },
+  mutations: {
+    setProfile(state, profile) {
+      state.profile = profile;
+    },
+  },
+  actions: {
+    async getProfile({ commit }) {
+      try {
+        let res = await api.get("profile");
+        commit("setProfile", res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+});

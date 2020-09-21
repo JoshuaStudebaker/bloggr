faaaf5e97260a3ef2cbf82945dee6a23448d5b35
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-20 19:46:14 -0600

    did some minor user blog card formatting

--- src/components/UserBlogsComponent.vue
+++ src/components/UserBlogsComponent.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="user-blogs-component col-4">
-    <div class="card card-style" style="width: 18rem;">
-      <div class="card-header justify-content-around">
-        <button type="button" class="btn btn-warning" @click="editPost(userBlogProp.id)">Edit</button>
-        <button type="button" class="btn btn-danger" @click="deletePost(userBlogProp.id)">Delete</button>
+  <div class="user-blogs-component col-md-4 my-3">
+    <div class="card card-style" style="height: 24rem;">
+      <div class="card-header justify-content-around" style="height: 5rem">
+        <button
+          type="button"
+          class="btn btn-warning"
+          @click="editPost(userBlogProp.id)"
+        >
+          Edit
+        </button>
+        <button
+          type="button"
+          class="btn btn-danger"
+          @click="deletePost(userBlogProp.id)"
+        >
+          Delete
+        </button>
       </div>
-      <div class="card-body">
-        <h5 class="card-title">{{userBlogProp.title}}</h5>
-        <h6 class="card-subtitle text-muted">{{userBlogProp.creator.name}}</h6>
-        <p class="card-text">{{userBlogProp.body}}</p>
+      <div class="card-body p-1" style="height: 15rem">
+        <h5 class="card-title">{{ userBlogProp.title }}</h5>
+        <h6 class="card-subtitle text-muted">
+          {{ userBlogProp.creator.name }}
+        </h6>
+        <p class="card-text">{{ userBlogProp.body }}</p>
       </div>
-      <div class="card-footer">
-        <router-link :to="{name:'blog-window', params: {blogId: userBlogProp.id}}">
+      <div class="card-footer" style="height: 4rem">
+        <router-link
+          :to="{ name: 'blog-window', params: { blogId: userBlogProp.id } }"
+        >
           <button class="btn btn-secondary">Click for Blog View</button>
         </router-link>
       </div>
@@ -43,5 +59,13 @@ export default {
   background-color: azure;
   border: 4px solid skyblue;
   box-shadow: 0px 0px 3px 3px cadetblue;
+}
+.card-body {
+  display: block;
+  text-overflow: ellipsis;
+  word-wrap: break-word;
+  overflow: hidden;
+  max-height: 15rem;
+  line-height: 1.5rem;
 }
 </style>

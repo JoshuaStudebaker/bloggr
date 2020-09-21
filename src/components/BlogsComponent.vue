9ab087d71f412316d1e2d97f955b32527a45b63e
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-20 19:30:02 -0600

    got blog cards really nice, accounts for overflow

--- src/components/BlogsComponent.vue
+++ src/components/BlogsComponent.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="blogs-component col-md-4 ">
+  <div class="blogs-component col-md-4 my-3">
     <router-link :to="{ name: 'blog-window', params: { blogId: blogProp.id } }">
-      <div class="card card-style m-2" style="height: 20rem">
-        <div class="card-header">
+      <div class="card card-style m-2" style="height: 25rem">
+        <div class="card-header" style="height: 5rem">
           <h5 class="card-title  ">{{ blogProp.title }}</h5>
         </div>
-        <div class="card-body shadow m-2">
+        <div class="card-body shadow">
           <p class="card-text card-body-style">{{ blogProp.body }}</p>
         </div>
-        <div class="card-footer">
+        <div class="card-footer" style="height: 3rem">
           <h6 class="card-subtitle text-muted">{{ blogProp.creator.name }}</h6>
         </div>
       </div>
@@ -35,15 +35,16 @@ export default {
   box-shadow: 0px 0px 3px 3px cadetblue;
 }
 
+.card-body {
+  padding: 0.85rem;
+}
+
 .card-body-style {
-  /* white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis; */
   display: block;
   text-overflow: ellipsis;
   word-wrap: break-word;
   overflow: hidden;
-  max-height: 90%;
-  line-height: 1.8em;
+  max-height: 15rem;
+  line-height: 1.5rem;
 }
 </style>
